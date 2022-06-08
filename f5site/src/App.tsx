cc1cb683056c3342e6a5948dc65d08b7c84b8ec0
unknown <akyla@fal.local>
2022-06-07 21:18:41 -0300

    Site F5

---
+++ f5site/src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./styles/Global";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+
+      <GlobalStyle/>
+    </div>
+  );
+}
